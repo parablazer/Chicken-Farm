fe17192b5428d339e03ca6b751c6bb4b047509db
parablazer <52578409+parablazer@users.noreply.github.com>
2019-07-14 19:02:22 -0500

    Update Chicken_Feeder.py

--- Chicken_Feeder.py
+++ Chicken_Feeder.py
@@ -1,1 +1,19 @@
+#This program will controll the feeders in the chicken run.
 
+import datetime, os, schedule, RPi.GPIO as gpio
+
+Sunrise = gpio.input(25, gpio.high)
+
+global TIME
+date = datetime.datetime.now()
+while True:
+        Feed_Chickens()
+
+
+
+def Feed_Chickens():
+    global TIME
+    gpio.output(18, gpio.high)
+    time.sleep(10)
+    gpio.output(18, gpio.low)
+    return;
